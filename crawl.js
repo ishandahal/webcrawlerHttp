6da1253369796de87956b05569167d334f8c6b82
ishan dahal <ishan.dahal88@gmail.com>
2023-05-02 09:42:17 -0400

    Add recursive crawling to each url

--- crawl.js
+++ crawl.js
@@ -1,37 +1,68 @@
-// const { JSDOM } = require("jsdom");
-import JSDOM from "jsdom";
+const { JSDOM } = require('jsdom');
+// import JSDOM from 'jsdom';
 
-async function crawlPage(url) {
-  console.log(`Actively crawling ${url}`);
+async function crawlPage(baseURL, currentURL, pages) {
+  // If this is on offsite URL, return immediately
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedURL = normalizeURL(currentURL);
+
+  // If we have already visited this page
+  // just increase the count and don't visit again
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++;
+    return pages;
+  }
+
+  // Initialize this page in the map
+  // since it doesn't exist yet
+  pages[normalizedURL] = 1;
+
+  console.log(`Actively crawling ${currentURL}`);
+
   try {
-    const resp = await fetch(url);
+    const resp = await fetch(currentURL);
 
     const status = resp.status;
 
     if (status > 399) {
-      console.log(`Error with fetch status code ${status} with ${url}`);
-      return;
+      console.log(`Error with fetch status code ${status} with ${currentURL}`);
+      return pages;
     }
 
-    const contentType = resp.headers.get("content-type");
+    const contentType = resp.headers.get('content-type');
 
-    if (!contentType.includes("text/html")) {
-      console.log(`Content type: ${contentType} in ${url}`);
-      return;
+    if (!contentType.includes('text/html')) {
+      console.log(`Content type: ${contentType} in ${currentURL}`);
+      return pages;
     }
-    console.log(await resp.text());
+
+    const htmlBody = await resp.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
-    console.log(`Error: ${err.message} with ${url}`);
+    console.log(`Error: ${err.message} with ${currentURL}`);
   }
+
+  return pages;
 }
 
 function getURLsFromHTML(HTMLbody, basePath) {
   const URLsArray = [];
+
   const dom = new JSDOM(HTMLbody);
-  const linkElements = dom.window.document.querySelectorAll("a");
+  const linkElements = dom.window.document.querySelectorAll('a');
 
   for (const link of linkElements) {
-    if (link.href.slice(0, 1) === "/") {
+    if (link.href.slice(0, 1) === '/') {
       // Relative url
       try {
         const urlObj = new URL(`${basePath}${link.href}`);
@@ -55,11 +86,11 @@ function getURLsFromHTML(HTMLbody, basePath) {
 function normalizeURL(urlString) {
   const urlObject = new URL(urlString);
   const hostPath = `${urlObject.hostname}${urlObject.pathname}`;
-  if (hostPath && hostPath.slice(-1) === "/") {
+  if (hostPath && hostPath.slice(-1) === '/') {
     return hostPath.slice(0, -1);
   }
   return hostPath;
 }
 
-// module.exports = { normalizeURL, getURLsFromHTML, crawlPage };
-export { crawlPage };
+module.exports = { normalizeURL, getURLsFromHTML, crawlPage };
+// export { crawlPage };
--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-import { crawlPage } from "./crawl.js";
+// import { crawlPage } from './crawl.js';
+const { crawlPage } = require('./crawl');
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
-    console.log("No url found.");
+    console.log('No url found.');
     process.exit(1);
   }
   if (process.argv.length > 3) {
-    console.log("Can only provide one url.");
+    console.log('Can only provide one url.');
     process.exit(1);
   }
-  const url = process.argv[2];
-  console.log(`Crawling ${url}`);
+  const baseURL = process.argv[2];
+  console.log(`Crawling ${baseURL}`);
 
-  crawlPage(url);
+  const pages = await crawlPage(baseURL, baseURL, {});
+
+  for (const page of Object.entries(pages)) {
+    console.log(page);
+  }
 }
 
 main();
